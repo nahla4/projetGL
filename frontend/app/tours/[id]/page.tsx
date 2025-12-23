745ba23c5f46b2d9e5e5cf1d8cec741c6526a0a2
Manar <m_nadji@estin.dz>
2025-12-23 21:24:24 +0100

    Update import path for Tour type in page.tsx

--- frontend/app/tours/[id]/page.tsx
+++ frontend/app/tours/[id]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import {useBookings, Tour as TourType } from "../booking/bookingcontext";
+import {useBookings, Tour as TourType } from "../booking/reserve/page";
 import "./tours.css";
 
 export type Tour = {
